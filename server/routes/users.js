7be0c0533257a0bcfc967976308751987897dd3f
Elif ztürk <nozturk@sabanciuniv.edu>
2022-11-01 15:25:00 +0300

    worked on post operation

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,54 @@
 var express = require("express");
 var router = express.Router();
+const userModel = require("../models/user.model");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+// /* GET users listing. */
+// router.get("/", function (req, res, next) {
+//   res.send("respond with a resource");
+// });
+
+router.post("/login", function (req, res) {
+  //check if the user registered before
+  var newUser = new userModel();
+  newUser.name = req.body.name;
+  newUser.lastname = req.body.lastname;
+  newUser.email = req.body.email;
+  newUser.password = req.body.password;
+  newUser.status = true;
+
+  newUser.save(function (err, data) {
+    if (err) {
+      console.log(error);
+    } else {
+      res.send("Data inserted");
+    }
+  });
+  //res.send("Login page");
 });
+
+router.get("/signup", function (req, res) {
+  res.send("Signup page");
+});
+
+router.get("/profile", function (req, res) {
+  res.send("Profile page");
+});
+
+// router.post("save", function (req, res) {
+//   var newUser = new userModel();
+//   newUser.name = req.body.name;
+//   newUser.lastname = req.body.lastname;
+//   newUser.email = req.body.email;
+//   newUser.password = req.body.password;
+
+//   newUser.save(function (err, data) {
+//     if (err) {
+//       console.log(error);
+//     } else {
+//       res.send("Data inserted");
+//     }
+//   });
+// });
 
 module.exports = router;
 
