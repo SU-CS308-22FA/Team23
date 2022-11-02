eb3824fac44c6c831622fd474acbf44546fcdadb
mgedikoglu <mustafaenes2012@gmail.com>
2022-11-03 01:16:01 +0300

    env file added

--- server/controller/mongoDB.controller.js
+++ server/controller/mongoDB.controller.js
@@ -26,12 +26,10 @@ async function run() {
   const conn = mongoose.createConnection();
   conn.model("User", userSchema);
 
+  const dbURL = process.env.DB_URL;
   // But call `mongoose.connect()`, which connects MongoDB's default
   // connection to MongoDB. `conn` is still disconnected.
-  await mongoose.connect(
-    "mongodb+srv://app:vbSczxBpNmkX5a4q@clustertff.h8qujjg.mongodb.net/?retryWrites=true&w=majority",
-    { dbName: "tff-project" }
-  );
+  await mongoose.connect(dbURL, { dbName: "tff-project" });
 }
 
 run().catch(console.error);
